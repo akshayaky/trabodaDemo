bc0fb3af6c87cbc8916ec6c022c6e5722532a54d
HrishikeshP-01 <62914161+HrishikeshP-01@users.noreply.github.com>
2021-06-21 22:19:13 +0530

    Added txt file reading feature

---
+++ Assets/Scripts/TextReader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextReader : MonoBehaviour
+{
+    [SerializeField]
+    string Filename = "";
+    [SerializeField]
+    int[] LineNumbers = { };
+    string RootFolder = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (File.Exists(Filename))
+        {
+            string[] lines = File.ReadAllLines(Filename);
+            string Question = "";
+            foreach(int number in LineNumbers)
+            {
+                Question += lines[number] + "\n";
+            }
+            GetComponent<Text>().text = Question;
+        }
+        else
+        {
+            GetComponent<Text>().text = "Question Not Found";
+        }
+    }
+}
